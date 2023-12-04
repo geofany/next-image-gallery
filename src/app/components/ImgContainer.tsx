2de6ebe445f90e5417c693e78d8614297a151390
geofany <geofanygalindra@gmail.com>
2023-12-04 14:33:49 +0700

    initial commit

---
+++ src/app/components/ImgContainer.tsx
@@ -0,0 +1,36 @@
+import type { Photo } from "@/models/Images";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+    photo: Photo
+}
+
+export default function ImgContainer({ photo }: Props) {
+
+    const widthHeightRatio = photo.height / photo.width
+    const galleryHeight = Math.ceil(250 * widthHeightRatio)
+    const photoSpans = Math.ceil(galleryHeight / 10) + 1
+
+    return (
+        <div
+          className="w-[250px] justify-self-center"
+          style={{ gridRow: `span ${photoSpans}` }}
+        >
+          <Link href={photo.url} target="_blank" className="grid place-content-center">
+            <div className="rounded-xl overflow-hidden group">
+              <Image
+                alt={photo.alt}
+                blurDataURL={photo.blurredDataUrl}
+                className="group-hover:opacity-75"
+                // placeholder="blur"
+                sizes="250px"
+                src={photo.src.large}
+                width={250}
+                height={galleryHeight}
+              />
+            </div>
+          </Link>
+        </div>
+    )
+}
---
+++ src/lib/getPrevNextPages.ts
@@ -0,0 +1,21 @@
+import type { ImagesResults } from "@/models/Images";
+
+function getPagesNumber(url: string) {
+  const { searchParams } = new URL(url)
+  return searchParams.get('page')
+}
+
+export default function getPrevNextPages(images: ImagesResults) {
+  let nextPage = images?.next_page ? getPagesNumber(images.next_page) : null
+  const prevPage = images?.prev_page ? getPagesNumber(images.prev_page) : null
+
+  const totalPages = images.total_results % images.per_page ? Math.ceil(images.total_results / images.per_page) : (images.total_results / images.per_page) + 1
+
+  if(prevPage && (parseInt(prevPage) + 5) < totalPages) {
+    nextPage = (parseInt(prevPage) + 5).toString()
+  } 
+
+  if (nextPage && (parseInt(nextPage) >= totalPages)) nextPage = null
+
+  return {prevPage, nextPage}
+}
